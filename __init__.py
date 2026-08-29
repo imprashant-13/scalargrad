54874f91d560131cb3e91ba59a1068f577c4a2bd
imprashant-13 <iamprashant2006@gmail.com>
2026-08-29 08:16:37 +0530

    add init

---
+++ __init__.py
@@ -0,0 +1,18 @@
+from .engine import Scalar
+
+from .nn import(
+    Neuron,
+    Layer,
+    MLP,
+)
+
+from .loss import(
+    MSELoss,
+    BCELoss,
+    CELoss,
+)
+
+from .optimizer import(
+    SGD,
+    zero_grad,
+)
